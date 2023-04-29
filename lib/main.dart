09f8ed2305153b362f816b3da3c20bcd722c3a52
gbrlcoelho <coelhogabriel67@gmail.com>
2023-04-29 09:29:43 -0300

    feat(main): init hive database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_list_flutter/pages/todo_list_page.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox('todo');
+
   runApp(const MyApp());
 }
 
